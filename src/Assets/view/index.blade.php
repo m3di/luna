ec567d8a2271cee0c36ee752fca35f7ed80acdf6
moham <mohamad-mehdi@live.com>
2021-07-07 20:19:51 +0430

    working on sidebar

--- src/Assets/view/index.blade.php
+++ src/Assets/view/index.blade.php
@@ -12,18 +12,30 @@
 
         @if(auth()->check())
             window.luna = @json(Luna::export());
-            window.lang = @json(__('luna'));
+        window.lang = @json(__('luna'));
         @endif
+
+        window.addEventListener('load', function (e) {
+            $('#msbo').on('click', function () {
+                $('body').toggleClass('msb-x');
+            });
+        })
     </script>
     <script src="/luna/js/index.js" defer></script>
 </head>
 <body class="rtl">
 <div id="app" style="display: none">
-    <nav class="navbar navbar-dark bg-dark navbar-expand-md fixed-top">
+    <nav class="mnb navbar navbar-dark bg-dark navbar-expand-md fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ ucfirst(config('app.name')) }}
-            </a>
+            <div class="navbar-header">
+                <a href="javascript:void(0)" id="msbo" class="text-muted">
+                    <i class="ic fa fa-bars"></i>
+                </a>
+
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ ucfirst(config('app.name')) }}
+                </a>
+            </div>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#app-navbar-collapse"
                     aria-controls="app-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,27 +68,25 @@
         </div>
     </nav>
 
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <nav class="col-md-3 col-lg-2 sidebar">
-                <div class="sidebar-sticky">
-                    <div class="user d-flex align-items-start">
-                        <div class="avatar">
-                            <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim(auth()->user()->email))) }}?s=40"
-                                 alt="">
-                        </div>
-                        <div>
-                            <div class="username">{{ auth()->user()->name }}</div>
-                            <div class="title">{{ auth()->user()->email }}</div>
-                        </div>
-                    </div>
-                    <luna-menu></luna-menu>
-                </div>
-            </nav>
-
-            <div role="main" class="col-md-9 col-lg-10 px-4 mb-md-5" :class="{'ml-md-auto': user != null}">
-                <router-view :key="$route.path"></router-view>
+    <nav class="msb">
+        <div class="user d-flex align-items-start">
+            <div class="avatar">
+                <img
+                        src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim(auth()->user()->email))) }}?s=40"
+                        alt="">
             </div>
+            <div>
+                <div class="username">{{ auth()->user()->name }}</div>
+                <div class="title">{{ auth()->user()->email }}</div>
+            </div>
+        </div>
+
+        <luna-menu></luna-menu>
+    </nav>
+
+    <div role="main" class="mcw px-4 mb-md-5">
+        <div class="cv">
+            <router-view :key="$route.path"></router-view>
         </div>
     </div>
 </div>
