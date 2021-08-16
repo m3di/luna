f1350eed4c9a7ee90a49337b1a1a2880bf30001c
moham <mohamad-mehdi@live.com>
2021-08-17 00:21:56 +0430

    fix rearrange query

--- src/Types/HasMany.php
+++ src/Types/HasMany.php
@@ -308,8 +308,8 @@ class HasMany extends Relation
 
     private function actionRearrange(Request $request, Resource $resource, Model $model)
     {
-        $query = call_user_func([$model, $this->getRelation()]);
         $rResource = $this->getRelationResource();
+        $query = $rResource->getQuery();
         $key = $rResource->getPrimaryKey();
         $title = is_callable($rResource->title) ? function ($item) use ($rResource) {
             return call_user_func($rResource->title, $item);
